a1d1ea9a8bddd6ce3aec0359c9ac4441cc2f475a
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-01-29 18:05:07 +0300

    feat: add cp and wt

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import path from 'path';
+import * as url from 'url';
+import childProcess from 'child_process';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+let child;
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToChildProcess = path.join(__dirname, './files/script.js');
+
+    child = childProcess.fork(pathToChildProcess, args, {
+        silent: true
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
+
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [1,'a',2,'B',3,'C']);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,54 @@
+import { Worker } from 'worker_threads';
+import path from 'path';
+import * as url from 'url';
+import os from 'os';
+//  тоже самое , что os.cpus  показывает  сколько ядер на компьютере
+// import {availableParallelism} from 'os';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const pathToWorkerThread = path.join(__dirname, './worker.js');
+
+
+
+const createWorker = (indexOfFibonacciToCalculate) => {
+    return new Worker(pathToWorkerThread, {
+        workerData: indexOfFibonacciToCalculate
+    });
+}
+
+const listenToWorkerMessage = (worker) => {
+    return new Promise((res) => {
+        worker.on('message', (message) => {
+            worker.terminate();
+            res({
+                status: 'resolved',
+                data: +message,
+            });
+        });
+        worker.on('error', () => {
+            res({
+                status: 'error',
+                data: null,
+            });
+        });
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const numberOfCpus = os.cpus().length;
+    const workersResultPromise = [];
+    let indexOfFibonacciToCalculate = 10;
+
+    for (let i = 0; i < numberOfCpus; i += 1) {
+        const worker = createWorker(indexOfFibonacciToCalculate);
+        workersResultPromise.push(listenToWorkerMessage(worker));
+
+        indexOfFibonacciToCalculate += 1;
+    }
+
+    Promise.all(workersResultPromise).then((workerResults) => {
+        console.log(workerResults);
+    });
 };
 
 await performCalculations();
